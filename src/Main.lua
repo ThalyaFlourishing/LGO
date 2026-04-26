8dab72ac7a449bd7c228d33c66bb0da872ce9058
--replace-all <ThalyaFlourishing@gmail.com>
2026-04-26 04:40:56 -0700

    Add comment

--- src/Main.lua
+++ src/Main.lua
@@ -2,4 +2,5 @@ import "Turbine";
 import "Turbine.Gameplay";
 
 -- Entry point for <Package>Thalya.lgo.Main</Package>
+-- The double extension is part of the LotRO naming convention, in which the name mirrors the folder structure
 import "Thalya.lgo.lgo";
